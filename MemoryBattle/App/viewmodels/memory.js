af2af3f87ad260b8d2e9db0ad8b4d47cfec5984a
Erik Magana <erik@simpleltc.com>
2013-10-26 17:49:56 -0500

    Game now duplicates results from flickr feed for tile use

--- MemoryBattle/App/viewmodels/memory.js
+++ MemoryBattle/App/viewmodels/memory.js
@@ -4,32 +4,34 @@
     //If you wish to be able to create multiple instances, instead export a function.
     //See the "welcome" module for an example of function export.
 
-    return {
-        images: [],
-        activate: function () {
+    var ctx = function(){
+        var that = this;
+        that.images = [];
+        that.activate = function () {
             //the router's activator calls this function and waits for it to complete before proceding
-            if (this.images.length > 0) {
+            if (that.images.length > 0) {
                 return;
             }
 
-            var that = this;
             return http.jsonp('http://api.flickr.com/services/feeds/photos_public.gne',
                     { /*tags: 'mount ranier', tagmode: 'any',*/ format: 'json' },'jsoncallback')
                 .then(function(response) {
                     var inUse = $.map(response.items.slice(0,4),
                                             function(ele, idx){
                                                 ele.show = false;
-                                                return ele;
+                                                return [ele, $.extend({}, ele)];
                                             });
-                     that.images = inUse;
+                     that.images = that.images.concat(inUse);
                 });
-        },
-        select: function(item) {
+        };
+        that.select = function(item) {
             item.show = !item.show;
-        },
-        canDeactivate: function () {
+        };
+        that.canDeactivate = function () {
             //the router's activator calls this function to see if it can leave the screen
             return app.showMessage('Are you sure you want to leave this page?', 'Navigate', ['Yes', 'No']);
-        }
+        };
     };
+
+    return ctx;
 });
