786f2af10d0fb5bc1425a441d3c8f2cb3709da6e
Erik Magana <erik@simpleltc.com>
2013-10-29 20:08:27 -0500

    increased timeout to allow users on slower machines a full second to view the tiles flipped over

--- MemoryBattle/App/viewmodels/memory.js
+++ MemoryBattle/App/viewmodels/memory.js
@@ -1,4 +1,5 @@
-﻿define(['plugins/http', 'durandal/app', 'extensions/arrays', 'knockout', '/signalr/hubs'], function (http, app, ko) {
+﻿define(['plugins/http', 'durandal/app', 'knockout', 'extensions/arrays', '/signalr/hubs'],
+    function (http, app, ko) {
     //Note: This module exports an object.
     //That means that every module that "requires" it will get the same object instance.
     //If you wish to be able to create multiple instances, instead export a function.
@@ -8,10 +9,10 @@
         var that = this;
         that.game = {};
         that.challengers = [];
-        var previousMatch;
+        that.previousMatch = undefined;
         var gameHubProxy = $.connection.game;
-        var gameID = $.connection.hub.id;
 
+        // SignalR listener for push changes from competitors
         gameHubProxy.client.challengersUpdate = function(game){
             var other = $.map(that.challengers, function(ele, idx){
                     if(game.Player === ele.Player){
@@ -25,6 +26,7 @@
              }
         };
 
+        // SignalR listener for push increases to our tile stack
         gameHubProxy.client.increaseStack = function(increment){
             if(that.game.Tiles <= 0)
                 return;
@@ -33,74 +35,81 @@
         };
 
         that.activate = function () {
-            //the router's activator calls this function and waits for it to complete before proceding
+            // make a call to the server to start the game
             return $.getJSON('/api/Game')
                 .then(startGame);
+        };
+
+        that.select = function(item) {
+            var pm = that.previousMatch;
+            item.show = !item.show;
+            if(pm && pm !== item){
+                setTimeout(function()
+                {
+                    matchAndUpdate.call(that, item, pm);
+                }, 1000);
+                that.previousMatch = undefined;
+            }else if(item.show){
+                that.previousMatch = item;
+            }else{
+                that.previousMatch = undefined;
+            }
+            that.updateState();
+        };
+
+        that.updateState = function(){
+            return gameHubProxy.server.gameUpdate(that.game);
         };
 
         function startGame (game){
             var tiles = game.Tiles - (game.Images.length > 0 ? game.Images.length / 2 : 0);
             that.game = game;
-
-            $.connection.hub.logging = true;
+            // $.connection.hub.logging = true;
             $.connection.hub.start();
             return updateTiles(tiles);
         }
 
         function updateTiles(tiles){
+            var flickr = 'http://api.flickr.com/services/feeds/photos_public.gne';
             var options = { tags: '', tagmode: 'any', format: 'json' };
-            return http.jsonp('http://api.flickr.com/services/feeds/photos_public.gne', options ,'jsoncallback')
+            return http.jsonp(flickr, options ,'jsoncallback')
                 .then(function(response) {
-                var inUse = $.map(response.items.slice(0, tiles),
-                    function(ele, idx){
-                        ele.show = false;
-                        return [ele, $.extend({}, ele)];
-                    });
-                that.game.Images = that.game.Images.concat(inUse).shuffle();
-                previousMatch = $.map(that.game.Images, function(ele, idx){ if(ele.show) return ele;})[0];
-                return updateState();
-            });
+                    var usefulTiles = response.items.slice(0, tiles);
+                    var tileSet = $.map(usefulTiles, makeDuplicate);
+                    that.game.Images = that.game.Images.concat(tileSet).shuffle();
+                    // this forces a full update otherwise there's jank
+                    that.game = that.game;
+                    that.previousMatch = $.map(that.game.Images, mapGetPreviousMatch)[0];
+                    return that.updateState();
+                });
         }
 
-        that.select = function(item) {
-            item.show = !item.show;
-            if(previousMatch && previousMatch !== item){
-                setTimeout(function()
-                {
-                    MatchAndUpdate.call(that, item);
-                }, 250);
-            }else if(item.show){
-                previousMatch = item;
-            }else{
-                previousMatch = undefined;
-            }
-            updateState();
-        };
-
-        function updateState(){
-            return gameHubProxy.server.gameUpdate(that.game);
+        function mapGetPreviousMatch(ele, idx){
+           if(ele.show)
+            return ele;
         }
 
-        function MatchAndUpdate(item){
+        function makeDuplicate(ele, idx){
+            ele.show = false;
+            return [ele, $.extend({}, ele)];
+        }
+
+        function matchAndUpdate(item, previousMatch){
             if(previousMatch.media.m === item.media.m){
                 this.game.Tiles--;
+                // SignalR push to increase everyone else's stack by 1
                 gameHubProxy.server.upStack(1);
+
                 this.game.Images = $.map(this.game.Images, function(ele, idx){
                     if(ele.media.m !== item.media.m)
                         return ele;
                 });
             }else{
                 item.show = false;
-               previousMatch.show = false;
+                previousMatch.show = false;
             }
-            previousMatch = undefined;
-            updateState();
+            this.updateState();
         }
-
-        that.canDeactivate = function () {
-            //the router's activator calls this function to see if it can leave the screen
-            return app.showMessage('Are you sure you want to leave this page?', 'Navigate', ['Yes', 'No']);
-        };
     };
 
     return ctx;
